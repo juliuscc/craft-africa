bf47121b84d678b19822dcb538892c1ddcb12836
JuliusCC <julius.cc@hotmail.com>
2017-04-16 20:17:28 +0200

    Initial commit

---
+++ config/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path') 
+
+const lintingLoader = {
+	test: /\.js?$/,
+	exlude: /node_modules/,
+	loader: 'eslint'
+}
+
+const babelLoader = {
+	test: /\.js?$/,
+	exlude: /node_modules/,
+	loader: 'babel-loader',
+	query: {
+		presets: ['es2015']
+	}
+}
+
+module.exports = {
+	entry: {
+		'bundle': [
+			path.resolve(__dirname, `../src/js/entry.js`)
+		],
+	},
+	output: {
+		path: path.resolve(__dirname, `../public/js`),
+		filename: '[name].js'
+	},
+	module: {
+		preLoaders: [lintingLoader],
+		loaders: [babelLoader]
+	}
+}
---
+++ gulpfile.js
@@ -0,0 +1,99 @@
+/* ~ ~ ~ INIT ~ ~ ~ */
+
+// Elementary objects
+const gulp = require('gulp')
+const gutil = require('gulp-util')
+const path = require('path')
+const plumber = require('gulp-plumber')
+const Cache = require('gulp-file-cache')
+const cache = new Cache()
+const nodemon = require('gulp-nodemon')
+
+// Paths and environment
+const paths = {
+	src: path.resolve(__dirname, 'src'),
+	dest: path.resolve(__dirname, 'public'),
+}
+const environments = require('gulp-environments')
+const development = environments.development
+const production = environments.production
+
+/* ~ ~ ~ Style ~ ~ ~ */
+const sass = require('gulp-sass')
+const autoprefixer = require('gulp-autoprefixer')
+const cleanCSS = require('gulp-clean-css')
+
+gulp.task('style', () => {
+	const src = `${paths.src}/stylesheets/main.sass`
+	const dest = `${paths.dest}/stylesheets`
+
+	return gulp.src(src)
+		.pipe(plumber(errorHandler))
+		.pipe(cache.filter())
+		.pipe(sass())
+		.pipe(autoprefixer())
+		.pipe(production(cleanCSS()))
+		.pipe(cache.cache())
+		.pipe(plumber.stop())
+		.pipe(gulp.dest(dest))
+})
+
+/* ~ ~ ~ JS / JSX ~ ~ ~ */
+const webpack = require('webpack-stream')
+const webpackConf = require('./config/webpack.config.js')
+const uglify = require('gulp-uglify')
+
+gulp.task('js', () => {
+	const src = `${paths.src}/js/**/*.js`
+	const dest = `${paths.dest}/js`
+
+	return gulp.src(src)
+		.pipe(plumber(errorHandler))
+		.pipe(webpack(webpackConf))
+		.pipe(production(uglify()))
+		.pipe(plumber.stop())
+		.pipe(gulp.dest(dest))
+})
+
+/* ~ ~ ~ Error Handler ~ ~ ~ */
+function errorHandler(err) {
+	const message = err.message.replace(/\n/g, '\n      ')
+
+	gutil.log('|- ' + gutil.colors.bgRed.bold('Build Error in ' + err.plugin))
+	gutil.log('|- ' + gutil.colors.bgRed('>>>'))
+	gutil.log('|\n    ' + message + '\n           |')
+	gutil.log('|- ' + gutil.colors.bgRed('<<<'))
+
+	this.emit('end')
+}
+
+/* ~ ~ ~ Stream and task handling ~ ~ ~ */
+gulp.task('watch', () => {
+	gulp.start('style', 'js')
+
+	// Style
+	gulp.watch(`${paths.src}/stylesheets/**/*.sass`, ['style'])
+
+	// JS / JSX
+	gulp.watch(`${paths.src}/js/**/*.js`, ['js'])
+})
+
+// Handling server
+gulp.task('nodemon', () => {
+	const stream = nodemon({
+		script: 'app.js',
+		watch: 'routes app.js'
+	})
+		.on('restart', () => {
+			gutil.log('restarted!')
+		})
+		.on('crash', () => {
+			gutil.log('Application has crashed!\n')
+			stream.emit('restart', 10)  // restart the server in 10 seconds 
+		})
+
+	return stream
+})
+
+gulp.task('default', ['nodemon', 'watch'])
+gulp.task('build', ['style', 'js'])
