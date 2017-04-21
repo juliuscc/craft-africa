3534720c423fba60b12c312c7715073b0471a3b7
Unknown <jkmalik@kth.se>
2017-04-21 16:56:42 +0200

    #6-email:

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 // const morgan = require('morgan')
+const mongoose = require('mongoose')
 
 // Routes
 const index = require('./routes/index')
@@ -39,17 +40,50 @@ app.use((req, res) => {
 	res.render('404')
 })
 
-// Error handling
+// Set Port
+app.set('port', (process.env.PORT || 5000))
+
+// Start server
+/* eslint-disable no-console */
+app.listen(app.get('port'), () => {
+	console.log(`Server started on port ${app.get('port')}`)
+})
+/* eslint-enable no-console */
+
+// Routes
+const index = require('./routes/index')
+const about = require('./routes/about')
+const contact = require('./routes/contact')
+const calculationForm = require('./routes/calculationform')
+const email = require('./routes/email')
+const handleTemplates = require('./routes/handletemplates')
+
+// Init App
+const app = express()
+
+// View Engine
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
+// BodyParser Middleware
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Router
+app.use('/', index)
+app.use('/about', about)
+app.use('/contact', contact)
+app.use('/calculationform', calculationForm)
+app.use('/email', email)
+app.use('/handletemplates', handleTemplates)
+
 app.use((req, res) => {
-	res.status(404)
 	res.render('404')
 })
-
-app.use((err, req, res, next) => {
-	res.status(500)
-	res.render('500')
-})
-
 
 // Set Port
 app.set('port', (process.env.PORT || 5000))
@@ -60,3 +94,14 @@ app.listen(app.get('port'), () => {
 	console.log(`Server started on port ${app.get('port')}`)
 })
 /* eslint-enable no-console */
+
+// Error handling
+app.use((req, res) => {
+	res.status(404)
+})
+	res.render('404')
+app.use((err, req, res, next) => {
+
+	res.render('500')
+	res.status(500)
+})
