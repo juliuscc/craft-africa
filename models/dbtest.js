da5a990580f0d9097bdc1738877e0795cce4cfc9
Thomas Peterson <thpeter@kth.se>
2017-04-21 14:40:31 +0200

    asdf

--- models/dbtest.js
+++ models/dbtest.js
@@ -1,9 +1,12 @@
 // Retrieve
-var MongoClient = require('mongodb').MongoClient;
+var Mongo = require('mongodb');
 
 // Connect to the db
-MongoClient.connect("mongodb://localhost:27017/exampleDb", function(err, db) {
+Mongo.MongoClient.connect("mongodb://localhost:27017/myTestDB", function(err, db) {
   if(!err) {
     console.log("We are connected");
+    var collection = db.collection("movie")
+
+    console.log(collection.find({}))
   }
 })
