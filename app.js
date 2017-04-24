873ed6e6f24a525ed6ef63a9b3dfeba957020f79
Unknown <jkmalik@kth.se>
2017-04-24 09:25:53 +0200

    #6-email: Added form for changing templates

--- app.js
+++ app.js
@@ -94,3 +94,100 @@ app.listen(app.get('port'), () => {
 	console.log(`Server started on port ${app.get('port')}`)
 })
 /* eslint-enable no-console */
+
+// Routes
+const index = require('./routes/index')
+const about = require('./routes/about')
+const contact = require('./routes/contact')
+const calculationForm = require('./routes/calculationform')
+const email = require('./routes/email')
+const handleTemplates = require('./routes/handletemplates')
+
+// Init App
+const app = express()
+
+// View Engine
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
+// BodyParser Middleware
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Router
+app.use('/', index)
+app.use('/about', about)
+app.use('/contact', contact)
+app.use('/calculationform', calculationForm)
+app.use('/email', email)
+app.use('/handletemplates', handleTemplates)
+
+app.use((req, res) => {
+	res.render('404')
+})
+
+// Set Port
+app.set('port', (process.env.PORT || 5000))
+
+// Start server
+/* eslint-disable no-console */
+app.listen(app.get('port'), () => {
+	console.log(`Server started on port ${app.get('port')}`)
+})
+/* eslint-enable no-console */
+
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+// const morgan = require('morgan')
+const mongoose = require('mongoose')
+
+// Routes
+const index = require('./routes/index')
+const about = require('./routes/about')
+const contact = require('./routes/contact')
+const calculationForm = require('./routes/calculationform')
+const email = require('./routes/email')
+const admin = require('./routes/admin')
+
+// Init App
+const app = express()
+
+// View Engine
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
+// BodyParser Middleware
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Router
+app.use('/', index)
+app.use('/about', about)
+app.use('/contact', contact)
+app.use('/calculationform', calculationForm)
+app.use('/email', email)
+app.use('/admin', admin)
+
+app.use((req, res) => {
+	res.render('404')
+})
+
+// Set Port
+app.set('port', (process.env.PORT || 5000))
+
+// Start server
+/* eslint-disable no-console */
+app.listen(app.get('port'), () => {
+	console.log(`Server started on port ${app.get('port')}`)
+})
+/* eslint-enable no-console */
