2ab50ad4f2e71ee1c26c4df5f58e3ecfeb19e78a
Unknown <jkmalik@kth.se>
2017-04-24 10:51:16 +0200

    #6-email Added initial database support (unfinished)

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,40 @@
 const router = require('express').Router()
+const mailTemplate = require('../models/mailTemplates')
 
-router.get('/', (req, res) => {
-	res.render('handletemplates', {
-		template: 'hello',
-		recipient: 'mail@foo.bar',
-		subject: 'a subject',
-		message: 'lorem ipsum dolore.......'
+router.get('/email', (req, res) => {
+	console.log('email')
+
+	res.render('handletemplates')
+
+	mailTemplate.getTemplateByName('seller', (err, template) => {
+		if (err){
+			console.log(err)
+			console.log('hgska')
+		}
+		else{
+			console.log(template)
+			console.log('hgsgdfsagrdgsjdjika')
+			res.render('handletemplates', {
+				name: template.name,
+				recipient: template.recipient,
+				subject: template.subject,
+				message: template.message
+			})
+		}
 	})
+
 })
 
-router.get('/edit', (req, res) => {
-	res.render('handletemplates')
+
+router.post('/email', (req, res) => {
+
+	const inputName = req.body.name
+
+	mailTemplate.update({name: inputName}, req.body, {upsert: true}, (err) => {
+		if(err){
+			console.log(err)
+		}
+	})
 })
 
 module.exports = router
