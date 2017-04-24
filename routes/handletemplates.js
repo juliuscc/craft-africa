b06854f1c03884fb3dccafc72430206d1120b942
Unknown <jkmalik@kth.se>
2017-04-24 08:30:36 +0200

    Form for editing templates

--- routes/handletemplates.js
+++ routes/handletemplates.js
@@ -2,27 +2,15 @@ const router = require('express').Router()
 
 router.get('/', (req, res) => {
 	res.render('handletemplates', {
-		template: 'tempy',
-		recipient: 'recy',
-		subject: 'suby',
-		message: 'messy'
+		template: 'hello',
+		recipient: 'mail@foo.bar',
+		subject: 'a subject',
+		message: 'lorem ipsum dolore.......'
 	})
 })
 
 router.get('/edit', (req, res) => {
 	res.render('handletemplates')
-})
-
-router.post('/edit', (req, res) => {
-	// Get form data
-	const { message, subject, email, templateName } = req.body
-
-	res.render('handletemplates', {
-		rmessage: `message is: ${message}`,
-		rsubject: `subject is: ${subject}`,
-		rrecipient: `recipeint mail is: ${email}`,
-		rtemplate: `template name is: ${templateName}`
-	})
 })
 
 module.exports = router
