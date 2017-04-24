ad34fd5e536531612f6423bf84b5c73e0a82ed1e
Fredrik <fredrik.lof.swe@gmail.com>
2017-04-24 14:43:32 +0200

    #6-email edit container stuff

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 
-router.get('/', (req, res) => {
+router.get('/handletemplates/', (req, res) => {
 	res.render('handletemplates', {
 		template: 'hello',
 		recipient: 'mail@foo.bar',
@@ -9,8 +9,51 @@ router.get('/', (req, res) => {
 	})
 })
 
-router.get('/edit', (req, res) => {
+router.get('/handletemplates/edit', (req, res) => {
 	res.render('handletemplates')
+})
+
+router.get('/calculationform/container', (req, res) => {
+	res.render('editcontainer', { containers: [
+		{
+			name: 'name',
+			type: 'type',
+			price: 'price',
+			size: 'size'
+		},
+		{
+			name: 'name',
+			type: 'type',
+			price: 'price',
+			size: 'size'
+		},
+		{
+			name: 'test',
+			type: 'test',
+			price: 'test',
+			size: 'test'
+		}
+	] })
+})
+
+router.post('/calculationform/container', (req, res) => {
+
+	const { name, type, price, size } = req.body
+	// Kinda like a for each loop
+	const containers = name.map((_, index) => {
+		return {
+			name: name[index],
+			type: type[index],
+			price: price[index],
+			size: size[index]
+		}
+	})
+	console.log(containers)
+	res.redirect('/admin/calculationform/container')
+})
+
+router.get('/calculationform/beertype', (req, res) => {
+	res.render('editbeertype')
 })
 
 module.exports = router
