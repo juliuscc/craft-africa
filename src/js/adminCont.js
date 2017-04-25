f436bf24a37e3daeb4e98e41b73868bdd12de02b
Unknown <julius.cc@hotmail.com>
2017-04-25 14:28:03 +0200

    Started with deleting rows

--- src/js/adminCont.js
+++ src/js/adminCont.js
@@ -1,3 +1,13 @@
+function removeRow(e) {
+	e.preventDefault()
+
+	const row = this.parentNode
+	const status = row.querySelector('[name="status"]')
+
+	row.classList.add('removed')
+	status.setAttribute('value', 'removed')
+}
+
 function addRow() {
 	// Get form
 	const form = document.querySelector('form')
@@ -9,6 +19,8 @@ function addRow() {
 	const priceInput = document.createElement('input')
 	const sizeInput = document.createElement('input')
 	const removeBtn = document.createElement('input')
+	const idInput = document.createElement('input')
+	const statusInput = document.createElement('input')
 
 	nameInput.setAttribute('type', 'text')
 	nameInput.setAttribute('name', 'name')
@@ -25,55 +37,30 @@ function addRow() {
 	removeBtn.setAttribute('type', 'submit')
 	removeBtn.setAttribute('class', 'remove')
 	removeBtn.setAttribute('value', '-')
+	removeBtn.addEventListener('click', removeRow)
+
+	idInput.setAttribute('type', 'hidden')
+	idInput.setAttribute('name', 'id')
+	idInput.setAttribute('value', undefined)
+
+	statusInput.setAttribute('type', 'hidden')
+	statusInput.setAttribute('name', 'status')
+	statusInput.setAttribute('value', 'new')
 
 	div.appendChild(nameInput)
 	div.appendChild(typeInput)
 	div.appendChild(priceInput)
 	div.appendChild(sizeInput)
 	div.appendChild(removeBtn)
+	div.appendChild(idInput)
+	div.appendChild(statusInput)
+
 	// Insert div
-
-	form.insertBefore(div, this)
-}
-function removeRow() {
-	const form = document.querySelector('form')
-
-	// Create div with inputs
-	const div = document.createElement('div')
-	const nameInput = document.createElement('input')
-	const typeInput = document.createElement('input')
-	const priceInput = document.createElement('input')
-	const sizeInput = document.createElement('input')
-	const removeBtn = document.createElement('input')
-
-	nameInput.setAttribute('type', 'text')
-	nameInput.setAttribute('name', 'name')
-
-	typeInput.setAttribute('type', 'text')
-	typeInput.setAttribute('name', 'type')
-
-	priceInput.setAttribute('type', 'text')
-	priceInput.setAttribute('name', 'price')
-
-	sizeInput.setAttribute('type', 'text')
-	sizeInput.setAttribute('name', 'size')
-
-	removeBtn.setAttribute('type', 'submit')
-	removeBtn.setAttribute('class', 'remove')
-	removeBtn.setAttribute('value', '-')
-
-	div.appendChild(nameInput)
-	div.appendChild(typeInput)
-	div.appendChild(priceInput)
-	div.appendChild(sizeInput)
-	div.appendChild(removeBtn)
-	// Insert div
-
 	form.insertBefore(div, this)
 }
 
 const addBtn = document.querySelector('#add')
 addBtn.addEventListener('click', addRow)
 
-const foo = document.querySelector('.remove')
-foo.addEventListener('click', removeRow)
+const removeBtns = document.querySelectorAll('.remove')
+removeBtns.forEach(btn => btn.addEventListener('click', removeRow))
