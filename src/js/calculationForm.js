be891e5ed0812e627b9a236673ae7b1eed22d3d1
Antonia Dåderman <antoniad@kth.se>
2017-04-27 13:16:56 +0200

    Started adding containers

--- src/js/calculationForm.js
+++ src/js/calculationForm.js
@@ -3,4 +3,20 @@ const containerCalculator = require('./calculation-form/containerCalculator')
 
 console.log(containerCalculator)
 
-containerCalculator.print()
+const data = {
+	litersOfBeer: 3000,
+	numberOfKegs: 30, //900L ändra till %
+	numberOfTap: 3540, //1770L ändra till %
+	numberOfBottles:100, //330L ändra till %
+	litersOfWater: 2000 //container capacity 5000L 
+
+}
+
+const  = {
+
+}
+
+containerCalculator.()
+
+
+console.log()
