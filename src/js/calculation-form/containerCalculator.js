1e0b43534e6ca6fecdc75f538e01493e2d59acb8
hannesrabo <hannes.rabo@gmail.com>
2017-04-27 11:21:02 +0200

    #2-calc-form-backend: Added example modules

--- src/js/calculation-form/containerCalculator.js
+++ src/js/calculation-form/containerCalculator.js
@@ -2,17 +2,6 @@ function print() {
 	console.log('calculating')
 }
 
-function inputCalculating (data) {
-
-		if (data.litersOfBeer < 3500)
-		return (container A) 
-		else is (data.litersOfBeer < 5000)
-			return (container B)
-		else(data.litersOfBeer < 8000)
-			return(container C)
-
-}
-
 module.exports = {
 	print: print
 }
