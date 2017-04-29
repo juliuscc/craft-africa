16d15ceb094bf623f1982b421308f59e492039ac
Unknown <julius.cc@hotmail.com>
2017-04-29 10:12:05 +0200

    Added pinned container

--- src/js/explosionView.js
+++ src/js/explosionView.js
@@ -1,4 +1,20 @@
-const scrollmagic = require('scrollmagic')
-const velocity = require('velocity-animate')
+const ScrollMagic = require('scrollmagic')
+// const velocity = require('velocity-animate')
 
-console.log(scrollmagic, velocity)
+const controller = new ScrollMagic.Controller()
+
+const windowHeight = document.querySelector('.explosion-view').offsetHeight
+const halveWindow = windowHeight / 2
+
+const sceneHeight = document.querySelector('.explosion-view-container').offsetHeight - windowHeight
+
+new ScrollMagic.Scene({
+	triggerElement: '.explosion-view-container',
+	offset: halveWindow,
+	duration: sceneHeight,
+	pushFollowers: false
+})
+.setPin('.explosion-view')
+.addTo(controller)
+
+// console.log(ScrollMagic, velocity)
