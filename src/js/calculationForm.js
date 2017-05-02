e14efa31d2a527fa5983695bf94fdfcaceeb7226
Thomas Peterson <thpeter@kth.se>
2017-05-02 14:26:40 +0200

    Swag is life

--- src/js/calculationForm.js
+++ src/js/calculationForm.js
@@ -1,10 +1,16 @@
 const containerCalculator = require('./calculation-form/mainCalculator')
+const containerCalculator2 = require('./calculation-form/containerCalculator')
 const ajax = require('./ajax')
-const Vue = require('vue')
+// const Vue = require('vue')
 
 const data = {
 	litersOfBeer: 3000,
 	numberOfKegs: 30, // 900L ändra till %
+	containerLiterAmount: {
+		tap: 5000,
+		bottle: 3000,
+		keg: 3000,
+	},
 	waterDistribution: {
 		numberOfTap: 0.4, // 1770L ändra till %
 		numberOfBottles: 0.2, // 330L ändra till %
@@ -31,11 +37,14 @@ ajax.loadJSON('/data/calculationStats')
 
 jsonCache.hej = ''
 
-const vob = new Vue({
-	el: 'body'
-})
+// const vob = new Vue({
+//	el: 'body'
+// })
 
-console.log(vob)
+// console.log(vob)
+console.log('Loaded calculation Form')
+
+containerCalculator2.getBeerProductionModules(data)
 
 document.querySelector('.calculation-form .calculate-button')
 		.addEventListener('click', () => {
