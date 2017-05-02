1d0ce6cc36e9e85eb42f97ac67fcbad10c53785f
Unknown <unknown@example.com>
2017-05-02 14:07:52 +0200

    #18-calculationFormDev: Bug fixes

--- routes/admin/helper.js
+++ routes/admin/helper.js
@@ -1,6 +1,6 @@
 function requiresAuth(req, res, target, options) {
 	if(req.user) {
-		res.render(`Admin/'${target}`, options)
+		res.render(`Admin/${target}`, options)
 	} else {
 		res.render('Admin/login', { user: req.user })
 	}
