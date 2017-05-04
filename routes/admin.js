d11526384094d308a7a65b2ec87c2499cf224bca
Unknown <julius.cc@hotmail.com>
2017-05-04 08:26:37 +0200

    Fixed wierd comment

--- routes/admin.js
+++ routes/admin.js
@@ -2,13 +2,13 @@ const router = require('express').Router()
 const passport = require('passport')
 const requiresAuth = require('./admin/helper')
 
-// // Get Routes
+// Get Routes
 const templatesRoute = require('./admin/templates')
 const containersRoute = require('./admin/containers')
 const beerRoute = require('./admin/beer')
 const emailRoute = require('./admin/email')
 
-// // Set Routes
+// Set Routes
 router.use('/emailtemplates', templatesRoute)
 router.use('/containers', containersRoute)
 router.use('/beer', beerRoute)
