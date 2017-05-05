fa7ff9820f89159e1d7746e36071230aa23d60a5
Thomas Peterson <thpeter@kth.se>
2017-05-05 11:48:19 +0200

    .

---
+++ routes/admin/statistics.js
@@ -0,0 +1,11 @@
+const router = require('express').Router()
+// const passport = require('passport')
+// const requiresAuth = require('./admin/helper')
+
+router.get('/', (req, res) => {
+	console.log("statistics route")
+	res.render('admin/statistics', { })
+})
+
+
+module.exports = router
