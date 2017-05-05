bc3d976d5211d535b94af61f12a4365d0631f10d
hannesrabo <hannes.rabo@gmail.com>
2017-05-05 12:46:37 +0200

    Added logger api

---
+++ models/loggerAPI.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const loggerSchema = mongoose.Schema({
+	type: {
+		type: String,
+		required: true
+	},
+	data: {
+		type: Object,
+		required: true
+	}
+})
+
+const Logger = mongoose.model('Logger', loggerSchema)
+module.exports = Logger
+
+module.exports.log = (logType, logData, callback) => {
+	const logEntry = new Logger({ type: logType, data: logData })
+	logEntry.save(callback)
+}
+
+module.exports.getLogByType = (logType, callback) => {
+	const query = {type: logType}
+	Logger.find(query, callback)
+}
