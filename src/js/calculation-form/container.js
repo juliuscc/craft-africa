f5c67c725102121d9ccc8cd5b280181f4cac138c
Unknown <unknown@example.com>
2017-05-08 13:47:31 +0200

    Finished container module

--- src/js/calculation-form/container.js
+++ src/js/calculation-form/container.js
@@ -3,12 +3,7 @@ _____________________________________________________________________________*/
 
 // Get the smallest production module for this beer production capacity
 function getProductionModules(stats) {
-	// const productionModules = stats.containers.production
-
-	const productionModules = [
-		{ name: 'A1', brewingCapacity: 9000, cost: 8000, fermenting: 5000 },
-		{ name: 'B1', brewingCapacity: 18000, cost: 12000, fermenting: 5000 }
-	]
+	const productionModules = stats.containers.production
 	const totalVolume = stats.volume.total
 	let returnModule
 	productionModules.forEach((container) => {
@@ -19,18 +14,14 @@ function getProductionModules(stats) {
 		}
 	})
 
-	// Shoud we return array instead?
 	return returnModule
 	// Hannes fix error handeling.
 }
 
 // Get the modules required for fermenting the beer
 function getFermentingModules(stats) {
-	const fermentingModules = [{ name: 'A2', cost: 4000, fermenting: 5000 },
-		{ name: 'A3', cost: 4000, fermenting: 5000 },
-		{ name: 'B2', cost: 6000, fermenting: 10000 },
-		{ name: 'B3', cost: 6000, fermenting: 15000 }
-	]
+	const fermentingModules = stats.containers.fermenting
+
 // Check needed capacity.
 	const chosenProductionModule = getProductionModules(stats)
 	const fermentingCapacityBrewery = chosenProductionModule.fermenting
@@ -42,7 +33,7 @@ function getFermentingModules(stats) {
 	const filteredFermentationModules = fermentingModules.filter(
 		element => element.name.charAt(0) === identifyingTypeChar)
 
-	let requiredFermentationModules = []
+	const requiredFermentationModules = []
 // Subtract fermenting capacity for each available module
 	filteredFermentationModules.forEach((container) => {
 		if(neededCapacity > 0) {
@@ -54,33 +45,46 @@ function getFermentingModules(stats) {
 	return requiredFermentationModules
 }
 
-// Get the addon modules
+// Get the addon modules C1 = keg storage and C2 is Bottle machine
 function getAddonsModules(stats) {
-	return stats
+	const addonModules = stats.containers.addons
+
+	const requiredAddonsModules = []
+	addonModules.forEach((container) => {
+		if(container.choosen) {
+			requiredAddonsModules.push(container)
+		}
+	})
+	return requiredAddonsModules
 }
 
 /* Public interface
 _____________________________________________________________________________*/
 
 // Calculates how much water the configuration cleans each month
-function getWaterCleaningCapacity(stats) {
+/* function getWaterCleaningCapacity(stats) {
+
+
 	return stats
 }
 
 // Caculate the enegey production from the solar panel
 function getEnergyProduction(stats) {
+
+
 	return stats
-}
+} */
 
 // Get a new container configuration
 // Calculate the list of all modules required for this production capacity
 function getConfiguration(stats) {
-	const production = getProductionModules(stats)
+	const production = [getProductionModules(stats)]
 	const fermenting = getFermentingModules(stats)
 	const addons = getAddonsModules(stats)
 
 	// concatinating arrays
-	const all = production.concat(fermenting).concat(addons)
+	// const all = production.concat(fermenting).concat(addons)
+	const all = (production.concat(fermenting)).concat(addons)
 
 	return { production, fermenting, addons, all }
 }
@@ -99,12 +103,18 @@ function getCost(stats) {
 
 module.exports = {
 	getConfiguration,
-	getWaterCleaningCapacity,
-	getEnergyProduction,
+	// getWaterCleaningCapacity,
+	// getEnergyProduction,
 	getCost
 }
 
 module.exports.test = function () {
-	return getFermentingModules({ volume: { total: 7000, relative: 18000 } })
+	return getConfiguration({ volume: { relative: 18000, total: 10000 },
+		containers: { production: [{ name: 'A1', brewingCapacity: 9000, fermenting: 5000 },
+		{ name: 'B1', brewingCapacity: 18000, fermenting: 5000 }],
+			fermenting: [{ name: 'B2', fermenting: 10000 }, { name: 'B3', fermenting: 15000 },
+	{ name: 'A2', fermenting: 5000 }, { name: 'A3', fermenting: 5000 }],
+			addons: [{ name: 'C1', type: 'addon', choosen: true }, { name: 'C2', type: 'addon', choosen: false }]
+		} })
 }
 
