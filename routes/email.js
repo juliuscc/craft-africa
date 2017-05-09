127f58801f3052b9891a7b933a484a0e0cab71dc
hannesrabo <hannes.rabo@gmail.com>
2017-05-09 11:12:58 +0200

    Added new sending system for emails to this branch

--- routes/email.js
+++ routes/email.js
@@ -6,13 +6,12 @@ const Handlebars = require('handlebars')
 
 // Render email send form
 router.get('/', (req, res) => {
-	console.log(req.user.local.name)
-	res.render('email', {username: req.user.local.name})
+	res.render('email')
 })
 
 // Render email send form
 router.get('/send', (req, res) => {
-	res.render('email', {username: req.user.local.name})
+	res.render('email')
 })
 
 // Send an email to admin and user.
@@ -20,8 +19,7 @@ router.post('/send', (req, res) => {
 	// Check for errors
 	if(!req.body.message_template) {
 		res.render('email', {
-			message: 'Something is wrong with the form',
-			username: req.user.local.name
+			message: 'Something is wrong with the form'
 		})
 		return
 	}
@@ -38,11 +36,10 @@ router.post('/send', (req, res) => {
 	}
 
 	// Querying database for info
-	mailTemplate.getTemplateByName(message_template, (databaseErr, template) => {
+	mailTemplate.getTemplateByName(req.body.message_template, (databaseErr, template) => {
 		if(databaseErr || !template) {
 			res.render('email', {
-				message: 'This link is currently not active. Please try another',
-				username: req.user.local.name
+				message: 'This link is currently not active. Please try another'
 			})
 		} else {
 			// recipients
@@ -69,14 +66,76 @@ router.post('/send', (req, res) => {
 				send(userEmailData, (errUser) => {
 					if(errAdmin || errUser) {
 						res.render('email', {
-							message: 'Something went wrong while trying to send email.',
-							username: req.user.local.name
+							message: 'Something went wrong while trying to send email.'
 						})
 					}
 
 					res.render('email', {
-						message: `Your email adress ${req.body.emailadress} was sent to a salesperson. We will be in touch shortly.`,
-						username: req.user.local.name
+						message: `Your email adress ${req.body.emailadress} was sent to a salesperson. We will be in touch shortly.`
+					})
+				})
+			})
+		}
+	})
+})
+
+router.post('/send/background', (req, res) => {
+	// Check for errors
+	if(!req.body.message_template) {
+		res.json({
+			json: 'Something is wrong with the form'
+		})
+		return
+	}
+
+	// Email credentials
+	const adminEmailData = {
+		user: emailcredentials.user,
+		pass: emailcredentials.pass
+	}
+
+	const userEmailData = {
+		user: emailcredentials.user,
+		pass: emailcredentials.pass
+	}
+
+	// Querying database for info
+	mailTemplate.getTemplateByName(req.body.message_template, (databaseErr, template) => {
+		if(databaseErr || !template) {
+			res.json({
+				error: 'This link is currently not active. Please try another'
+			})
+		} else {
+			// recipients
+			adminEmailData.to = template.recipient
+			userEmailData.to = req.body.emailadress
+
+			// Subjects
+			adminEmailData.subject = template.admin_subject
+			userEmailData.subject = template.user_subject
+
+			// message (compile handlebars code)
+			const adminMessageCompiled = Handlebars.compile(template.admin_message)
+			const userMessageCompiled = Handlebars.compile(template.user_message)
+
+			adminEmailData.html = adminMessageCompiled(req.body)
+			userEmailData.html = userMessageCompiled(req.body)
+
+			// Sending emails
+			const send = gmail()
+
+			// Admin email
+			send(adminEmailData, (errAdmin) => {
+				// User email
+				send(userEmailData, (errUser) => {
+					if(errAdmin || errUser) {
+						res.json({
+							error: 'Something went wrong while trying to send email.'
+						})
+					}
+
+					res.json({
+						message: `Your email adress ${req.body.emailadress} was sent to a salesperson. We will be in touch shortly.`
 					})
 				})
 			})
