bee01eda36547802c7b15a4b95dacef7712a0e96
hannesrabo <hannes.rabo@gmail.com>
2017-05-09 11:00:33 +0200

    Added json to route

--- routes/email.js
+++ routes/email.js
@@ -79,4 +79,68 @@ router.post('/send', (req, res) => {
 	})
 })
 
+router.post('/send/background', (req, res) => {
+	// Check for errors
+	if(!req.body.message_template) {
+		res.json({
+			json: 'Something is wrong with the form'
+		})
+		return
+	}
+
+	// Email credentials
+	const adminEmailData = {
+		user: emailcredentials.user,
+		pass: emailcredentials.pass
+	}
+
+	const userEmailData = {
+		user: emailcredentials.user,
+		pass: emailcredentials.pass
+	}
+
+	// Querying database for info
+	mailTemplate.getTemplateByName(req.body.message_template, (databaseErr, template) => {
+		if(databaseErr || !template) {
+			res.json({
+				error: 'This link is currently not active. Please try another'
+			})
+		} else {
+			// recipients
+			adminEmailData.to = template.recipient
+			userEmailData.to = req.body.emailadress
+
+			// Subjects
+			adminEmailData.subject = template.admin_subject
+			userEmailData.subject = template.user_subject
+
+			// message (compile handlebars code)
+			const adminMessageCompiled = Handlebars.compile(template.admin_message)
+			const userMessageCompiled = Handlebars.compile(template.user_message)
+
+			adminEmailData.html = adminMessageCompiled(req.body)
+			userEmailData.html = userMessageCompiled(req.body)
+
+			// Sending emails
+			const send = gmail()
+
+			// Admin email
+			send(adminEmailData, (errAdmin) => {
+				// User email
+				send(userEmailData, (errUser) => {
+					if(errAdmin || errUser) {
+						res.json({
+							error: 'Something went wrong while trying to send email.'
+						})
+					}
+
+					res.json({
+						message: `Your email adress ${req.body.emailadress} was sent to a salesperson. We will be in touch shortly.`
+					})
+				})
+			})
+		}
+	})
+})
+
 module.exports = router
