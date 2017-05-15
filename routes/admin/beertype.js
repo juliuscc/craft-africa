f845ba215ee979124b7d77774a57daad43b360de
Fredrik <fredrik.lof.swe@gmail.com>
2017-05-15 14:30:01 +0200

    BeerTypeFix

--- routes/admin/beertype.js
+++ routes/admin/beertype.js
@@ -1,16 +1,18 @@
 const router = require('express').Router()
 const auth = require('./auth')
-const beerType = require('../../models/beerTypeAPI')
+const beerTypesModule = require('../../models/beerTypeAPI')
 
 router.get('/', (req, res) => {
+	// console.log('test')
 	// Run if logged in
 	auth.runIfAdmin(req, res, () => {
-		beerType.getAllBeers((err, values) => {
+		beerTypesModule.getAllBeers((err, beerTypes) => {
+			// console.log('beer: ', beerTypes)
 			if(err) {
 				res.render('admin/errormessage', { message: err, username: req.user.local.name })
-				throw err
+			} else {
+				res.render('admin/beertype', { beerTypes, username: req.user.local.name })
 			}
-			res.render('admin/beertype', { values, username: req.user.local.name })
 		})
 	})
 })
@@ -19,30 +21,55 @@ router.post('/', (req, res) => {
 	// Require login
 	auth.runIfAdmin(req, res, () => {
 		if(!req.body.name) {
-			// Redirect if no data is submitted
 			res.redirect('/admin/beertype')
 		} else {
+			if(req.body.name.constructor !== Array) {
+				req.body.id = [req.body.id]
+				req.body.name = [req.body.name]
+				req.body.fermentingTime = [req.body.fermentingTime]
+				req.body.hops = [req.body.hops]
+				req.body.malt = [req.body.malt]
+				req.body.co2 = [req.body.co2]
+				req.body.status = [req.body.status]
+			}
 			const { id, name, fermentingTime, hops, malt, co2, status } = req.body
-			const ingredient = JSON.stringify({ hops, malt, co2 })
-			if(status === 'removed') {
-				beerType.removeBeer(id, (err) => {
+
+			const beerTypes = name.map((_, index) => ({
+				id: id[index],
+				name: name[index],
+				fermentingTime: fermentingTime[index],
+				ingredient: JSON.stringify({
+					hops: hops[index],
+					malt: malt[index],
+					co2: co2[index]
+				}),
+				status: status[index]
+			}))
+
+			const editedBeerTypes = beerTypes.filter(beerType => beerType.status === 'edited')
+			const removedBeerTypes = beerTypes.filter(beerType => beerType.status === 'removed')
+
+			removedBeerTypes.forEach((element) => {
+				beerTypesModule.removeBeer(element.id, (err) => {
 					if(err) {
-						res.render('admin/errormessage', { message: err, username: req.user.local.name })
+						res.render('admin/errormessage', { message: err })
 						throw err
 					}
 				})
-			} else {
-				beerType.updateBeerById(id, {
-					name,
-					fermentingTime,
-					ingredient
+			})
+
+			editedBeerTypes.forEach((element) => {
+				beerTypesModule.updateBeerById(element.id, {
+					name: element.name,
+					fermentingTime: element.fermentingTime,
+					ingredient: element.ingredient
 				}, (err) => {
 					if(err) {
-						res.render('admin/errormessage', { message: err, username: req.user.local.name })
+						res.render('admin/errormessage', { message: err })
 						throw err
 					}
 				})
-			}
+			})
 			res.redirect('/admin/beertype')
 		}
 	})
@@ -55,8 +82,6 @@ router.post('/new', (req, res) => {
 			// Redirect if no data is submitted
 			res.redirect('/admin/beertype')
 		} else {
-
-			console.log(req.body)
 			if(req.body.name.constructor !== Array) {
 				req.body.id = [req.body.id]
 				req.body.name = [req.body.name]
@@ -67,28 +92,26 @@ router.post('/new', (req, res) => {
 				req.body.status = [req.body.status]
 			}
 			const { id, name, fermentingTime, hops, malt, co2, status } = req.body
+
 			const beers = name.map((_, index) => ({
 				id: id[index],
 				name: name[index],
 				fermentingTime: fermentingTime[index],
-				hops: hops[index],
-				malt: malt[index],
-				co2: co2[index],
+				ingredient: JSON.stringify({
+					hops: hops[index],
+					malt: malt[index],
+					co2: co2[index]
+				}),
 				status: status[index]
 			}))
 
-			console.log(beers)
+			console.log('beeeeers: ', beers)
 
 			beers.forEach((element) => {
-				const ingredient = JSON.stringify({
-					hops: element.hops,
-					malt: element.malt,
-					co2: element.co2
-				})
-				beerType.createBeer({
+				beerTypesModule.createBeer({
 					name: element.name,
 					fermentingTime: element.fermentingTime,
-					ingredient
+					ingredient: element.ingredient
 				}, (err) => {
 					if(err) {
 						res.render('admin/errormessage', { message: err, username: req.user.local.name })
