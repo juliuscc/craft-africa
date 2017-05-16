0d007df5d63e73d32b681d593a4d60f136c36343
Thomas Peterson <thpeter@kth.se>
2017-05-16 14:17:12 +0200

    Statistics now require authentication + cleaning

--- routes/admin/helper.js
+++ routes/admin/helper.js
@@ -1,6 +1,5 @@
 function requiresAuth(req, res, target, options) {
 	if(req.user) {
-		console.log(req.user.local.name)
 		options.username = req.user.local.name
 		res.render(`admin/${target}`, options)
 	} else {
