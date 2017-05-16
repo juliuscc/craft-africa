fee382b7f120a71106032e0299e61dcb6bd70b38
Fredrik <fredrik.lof.swe@gmail.com>
2017-05-16 10:30:00 +0200

    templates bug fixed

--- src/js/adminDefaultValues.js
+++ src/js/adminDefaultValues.js
@@ -52,5 +52,5 @@ function rowEdited(e) {
 const editedRow = document.querySelectorAll('.validate')
 editedRow.forEach(textbox => textbox.addEventListener('change', rowEdited))
 
-const hundred = document.querySelectorAll('.hundred')
-hundred.forEach(textbox => textbox.addEventListener('change', isHundred))
+/* const hundred = document.querySelectorAll('.hundred')
+hundred.forEach(textbox => textbox.addEventListener('change', isHundred))*/
