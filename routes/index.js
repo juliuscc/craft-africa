b55c7b6fb9fded5c3b18fc190f6bc3ef5849302b
hannesrabo <hannes.rabo@gmail.com>
2017-05-17 17:39:28 +0200

    Updated for only explorer

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const browserCheck = require('./helper/browserCheck')
 
 router.get('/', (req, res) => {
-	const isSupportedBrowser = browserCheck.isSupportedBrowser(req)
+	const isSupportedBrowser = browserCheck.isNotExplorer(req)
 	res.render('index', { title: 'Home', isSupportedBrowser })
 })
 
